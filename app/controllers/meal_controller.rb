7bf06c860ac131b33cbb1fffdb4fd761a38ed54a
Tanzeel Khan <ktanzeel80@gmail.com>
2022-07-12 23:41:19 +0530

    Adding meal plan purchase option

--- app/controllers/meal_controller.rb
+++ app/controllers/meal_controller.rb
@@ -1,2 +1,23 @@
 class MealController < ApplicationController
+  def create
+    price = 2000
+    breakfast = params[:breakfast]
+    lunch = params[:lunch]
+    dinner = params[:dinner]
+    if breakfast != nil
+      price += 2000
+    elsif lunch != nil
+      price += 2000
+    elsif dinner != nil
+      prices += 2000
+    elsif tiffin != nil
+      price += 500
+    end
+    session[:price] = price
+    redirect_to new_meal_path
+  end
+
+  def new
+    render "mealconf"
+  end
 end
